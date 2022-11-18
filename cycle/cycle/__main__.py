ce83e8376740a45801ad8c92175e1a30869a6821
Alasa <ogboanohrichardjamb2020@gmail.com>
2022-11-18 11:47:57 +0100

    I tried to fix the scores problem but not tested

--- cycle/cycle/__main__.py
+++ cycle/cycle/__main__.py
@@ -1,9 +1,11 @@
 import constants
 
 from game.casting.cast import Cast
-from game.casting.object import Object #changed from game.casting.food import food
+# changed from game.casting.food import food
+from game.casting.object import Object
 from game.casting.score import Score
-from game.casting.cycles import Cycles #changed from game.casting.snake import snake
+# changed from game.casting.snake import snake
+from game.casting.cycles import Cycles
 from game.scripting.script import Script
 from game.scripting.control_actors_action import ControlActorsAction
 from game.scripting.move_actors_action import MoveActorsAction
@@ -17,35 +19,40 @@ from game.shared.point import Point
 
 
 def main():
-    #create the cast
+    # create the cast
 
     # Create two players, get their position and color
-    #Added code to create another player, get their position, and manipulate their cycle colors, all this is not included in snake
+    # Added code to create another player, get their position, and manipulate their cycle colors, all this is not included in snake
     cycle1 = Cycles(int(constants.MAX_X - 645), int(constants.MAX_Y / 2))
     cycle2 = Cycles(int(constants.MAX_X - 300), int(constants.MAX_Y / 2))
 
     cycle1.set_color(constants.GREEN)
     cycle2.set_color(constants.RED)
 
-    #I think the problem with the score might be here, because it already adds 5 points.
-    # player 1 
+    # I think the problem with the score might be here, because it already adds 5 points.
+    # I don't know if this fixes problem, I can't really test it because my system graphics card
+    # can't run the game so I need someone to test it out
+    # player 1
     cast = Cast()
-    score1 = Score() #Added this, not in snake
-    score1.add_points(5) #Added this, not in snake
-    cast.add_actor(constants.CYCLE_GROUP, cycle1) #changed from foods to cycle
-    cast.add_actor(constants.SCORE_GROUP, score1) #changed from scores to score1
-    score1.set_position(Point(constants.MAX_X+150, 10)) #added location points
+    score1 = Score(0)  # Added this, not in snake
+    score1.add_points(5)  # Added this, not in snake
+    # changed from foods to cycle
+    cast.add_actor(constants.CYCLE_GROUP, cycle1)
+    # changed from scores to score1
+    cast.add_actor(constants.SCORE_GROUP, score1)
+    score1.set_position(Point(constants.MAX_X+150, 10)
+                        )  # added location points
 
-    #added all of this for second cycle, not included in snake
-    # player 2 
-    score2 = Score()
+    # added all of this for second cycle, not included in snake
+    # player 2
+    score2 = Score(0)
     score2.add_points(5)
     cast.add_actor(constants.CYCLE_GROUP, cycle2)
     cast.add_actor(constants.SCORE_GROUP, score2)
     score2.set_position(Point(constants.MAX_X-200, 10))
 
     cast.add_actor(constants.OBJECT_GROUP, Object())
-   
+
     # start the game
     keyboard_service = KeyboardService()
     video_service = VideoService()
@@ -55,7 +62,7 @@ def main():
     script.add_action("update", MoveActorsAction())
     script.add_action("update", HandleCollisionsAction())
     script.add_action("output", DrawActorsAction(video_service))
-    
+
     director = Director(video_service)
     director.start_game(cast, script)
 
